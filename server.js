cea956b4ce64e9fc5531b6cb577ec5b3b83808ab
Rutvik Kapade <rutvik609@gmail.com>
2021-08-20 18:04:34 +0530

    one more

--- server.js
+++ server.js
@@ -18,38 +18,51 @@ var con = mysql.createConnection({
   password: "rootroot"
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
 
-app.get('/getCategories',(req,res)=>{
+
+app.get('/getCategories',async(req,res)=>{
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
 con.query('Select * from category',(error,result)=>{
 if(error){
 console.log(error);
 }else{
 console.log(result);
+con.end();
 res.json(result);
 }
 });
 })
 
 app.post('/getProductType',(req,res)=>{
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
 con.query('select * from product_type where category_name="'+req.body.category+'"',(error,result)=>{
 if(error){
  res.status(500).send('Something broke!');
 }else{
+con.end();
 res.json(result);
 }
 })
 })
 
 app.post('/sendData',(req,res)=>{
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
 con.query('INSERT INTO products (name, price,productImg,productType,brand) VALUES ("'+req.body.productName+'",'+req.body.productPrice+',"'+req.body.imageUrl+'","'+req.body.productType+'","'+req.body.productBrand+'");',(err,result)=>{
 if(err){
 console.log(err);
+con.end()
 res.status(501).send('Not Implemented!');
 }else{
+  con.end();
 res.status(200).send('Inserted Record!');
 }
 });
@@ -57,19 +70,26 @@ res.status(200).send('Inserted Record!');
 });
 
 app.post('/getProducts',(req,res)=>{
-console.log(req.body);
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
 if(req.body.data=='All'){
 con.query('select * from products inner join product_type on products.productType=product_type.product_type_name;',(err,result)=>{
 if(err){
+  con.end();
 console.log(err);
 }else{
+  con.end();
 res.json(result);
 }
 })}else{
 con.query('select * from products inner join product_type on products.productType=product_type.product_type_name where product_type.category_name="'+req.body.data+'";',(err,result)=>{
 if(err){
+  con.end()
 console.log(err);
 }else{
+  con.end();
 res.json(result);
 }})
 
